d589ee4631335cca925e13573306149d3b467311
JRyanHenson <hensojr@gmail.com>
2017-08-05 18:51:36 -0600

    Added js events.

--- index.php
+++ index.php
@@ -2,49 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link type="text/css" href="css/styles.css">
-		<link type="text/css" href="css/javascript-events.css">
-		<script src="/js/javascript-events.js" type="text/javascript"></script>
+		<link type="text/css" href="css/styles.css" rel="stylesheet">
+		<link type="text/css" href="css/javascript-events.css" rel="stylesheet">
+		<script src="js/javascript-events.js" type="text/javascript"></script>
 		<title>JavaScript Challenge</title>
 	</head>
 	<body>
 		<h1 class="text-center">This is my JavaScript challenge</h1>
-			<p>Tiramisu dessert cookie gummi bears icing biscuit dragée donut gummi bears. Dragée donut icing pastry
-						marzipan cookie jelly beans. Tiramisu sesame snaps sweet roll tootsie roll jelly beans chocolate cake
-						dessert chocolate bar apple pie.
-						Cupcake soufflé gummi bears lemon drops carrot cake candy canes ice cream jelly lollipop. Cookie
-						topping apple pie. Icing jelly toffee.
-						Cake chupa chups icing dragée marshmallow jelly-o. Lemon drops chocolate cake tart brownie sesame
-						snaps topping soufflé gummies dessert. Marzipan toffee cupcake ice cream pastry ice cream tiramisu.
-						Tart soufflé donut biscuit biscuit brownie. Dessert tart jelly pastry chocolate bar sweet. Macaroon
-						topping bear claw pudding dragée chupa chups gingerbread chupa chups caramels. Dessert caramels candy
-						canes.
-						Dessert bonbon topping gingerbread. Lemon drops pudding jelly pie soufflé cupcake. Muffin pastry
-						icing.</p>
-			<p>TEst sugar, medium foam java variety cortado fair trade strong. In instant, black sweet fair trade
-						aroma id redeye chicory. Strong dark, decaffeinated grinder a turkish flavour roast to go. Coffee
-						galão, robusta barista caramelization mocha grinder. Cinnamon single shot to go caramelization pumpkin
-						spice, turkish percolator grounds redeye americano.
-
-						Instant, organic café au lait crema, and carajillo cream flavour crema. Flavour cinnamon, eu,
-						extraction, affogato half and half milk carajillo cream crema. Whipped redeye, mocha, extra, cup
-						cultivar, caramelization pumpkin spice body robust americano. Percolator cultivar sweet, so beans iced
-						acerbic affogato grinder. Chicory rich wings single shot et galão est irish.
-
-						Froth skinny, strong that decaffeinated cream roast irish strong. To go, plunger pot, wings
-						frappuccino, foam, aftertaste americano pumpkin spice steamed aged brewed. Irish decaffeinated siphon
-						mocha, at, sweet, flavour macchiato brewed roast arabica plunger pot. Est viennese skinny, ristretto
-						bar ristretto at sit wings extra foam. To go beans café au lait arabica french press steamed mazagran
-						steamed shop.
-
-						Foam crema siphon to go aftertaste rich that black java. Lungo cappuccino, dripper doppio cinnamon est
-						cup spoon wings chicory cinnamon viennese. Cultivar trifecta plunger pot, pumpkin spice eu extraction
-						french press percolator spoon pumpkin spice. Body milk, sugar cappuccino espresso doppio grinder roast
-						aged. Mocha lungo crema con panna organic blue mountain milk frappuccino siphon.</p>
-			<p>TVicious and viscous, this menacing brew pours opaque black with a creamy maduro-colored head. Its
-						aroma offers seductive whiskey, chewy red wine, dark fruit and lavish tobacco. Berserker Imperial
-						Stout invades your taste buds with in-your-face flavor. Weighing in at almost 13% alcohol by volume,
-						Berserker is completely out-of-control. Give it a good fight. Berserker Imperial Stout was aged in
-						both red wine and whiskey barrels.</p>.
+			<h2>On Click</h2>
+			<p>What happens when you click here?</p>
+			<div id="click" onclick="clickEvent();">Nice!</div>
+			<p></p>.
 	</body>
 </html>
