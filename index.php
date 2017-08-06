7e5b6de80cafbd8e65e6f22f45b27221ad548568
JRyanHenson <hensojr@gmail.com>
2017-08-05 22:24:52 -0600

    Added find and replace funtiona and html.

--- index.php
+++ index.php
@@ -36,9 +36,16 @@
 			</p>
 		</div>
 		<h2>Find and Replace</h2>
-		<p>What happens when you...</p>
-		<div id="doubleClick" ondblclick="doubleClick();">
-
+		<div id="searchText">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+			dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</div>
+		<br>
+			<p>Find: <input id="find" name="find" type="text"</p>
+			<p>Replacment: <input id="replacement" name="replacement" type="text"</p>
+		<br>
+		<p><input type="button" value="Find" onClick="findMyText(document.getElementById('find').value, document.getElementById('replacement').value);"></p>
 	</body>
 </html>
--- js/javascript-events.js
+++ js/javascript-events.js
@@ -24,6 +24,24 @@ function mouseOut() {
 function doubleClick() {
 	document.getElementById("doubleClick").style.fontFamily = "Arial Hebrew";
 }
+
+/**
+ * finds value then replaces with if replacement is given. If no replacement is given highlights searched text
+ */
+function findMyText(find, replacement) {
+	var searchText = document.getElementById("searchText").innerHTML;
+	var match = new RegExp(find, "ig");
+	var replaced = "";
+
+	if (replacement.length > 0) {
+		replaced = searchText.replace(match, replacement);
+	}
+	else {
+		var boldText = "<div style=\"background-color: yellow; display: inline; font-weight: bold;\">" + find + "</div>";
+		replaced = searchText.replace(match, boldText);
+	}
+	document.getElementById("searchText").innerHTML = replaced;
+}
 /**
  * hides/shows the textarea when the checkbox is unchecked/checked
  **/
